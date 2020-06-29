ce96242e788cc9aecc1ee64acd7253f4b2f64964
Vladyslav <ganushkevychv@gmail.com>
2020-06-29 18:40:36 +0200

    corrections added to webmanifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `src/images/password.png`, // This path is relative to the root of the site.
+        icon: `src/images/password.png`,
       },
     },
     `gatsby-plugin-offline`,
